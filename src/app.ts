95023ab75e3ba2147d36def87da700280db13a0d
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2025-06-30 14:44:47 +0900

    feat: develop Socet.io Server

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,27 @@
 import cors from "cors";
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
-import cookieParser from 'cookie-parser';
-import { MORGAN_FORMAT } from './libs/config';
+import cookieParser from "cookie-parser";
+import { MORGAN_FORMAT } from "./libs/config";
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+import { T } from "./libs/types/common";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
 
-import session from 'express-session';
-import ConnectMongoDB from 'connect-mongodb-session';
-import { T } from './libs/types/common';
 const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
-    uri: String(process.env.MONGO_URL),
-    collection: "sessions",
-  });
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
 /** 1- ENTRANCE **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("./uploads"));
-app.use(express.urlencoded({extended: true})); // html formatdan kelgan malumotlarga qabul qiladi
+app.use(express.urlencoded({ extended: true })); // html formatdan kelgan malumotlarga qabul qiladi
 app.use(express.json()); //
 app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser()); // hosil qilgan tokkenimizni brauzerni cookisiga saxranit qilishga
@@ -27,26 +29,44 @@ app.use(morgan(MORGAN_FORMAT));
 
 /** 2- SESSIONS **/
 app.use(
-    session({
-        secret:String(process.env.SESSION_SECRET), // => secretni .env filedan olyapmiz
-  cookie: {
-    maxAge: 1000 * 3600* 6, // 6 soat => cookini qancha vaqt saqlanishini belgilaymiz
-  },
-  store: store, //=> store ga biz yuqorida yasagan mongoDB dagi sessions collectionini beryapmiz.
-  resave: true,    // => cookimiz 6 soat mobaynida saqlanar edi agar "resave : true " bolsa shu oraliqda user kirgan bolsa kirgan vaqtidan yana 6 soat hisob lanadi. folse bolsa ozgarmasdan birinchi kirgan vaqtidan hisoblanadi
-  saveUninitialized: true
-    })
+  session({
+    secret: String(process.env.SESSION_SECRET), // => secretni .env filedan olyapmiz
+    cookie: {
+      maxAge: 1000 * 3600 * 6, // 6 soat => cookini qancha vaqt saqlanishini belgilaymiz
+    },
+    store: store, //=> store ga biz yuqorida yasagan mongoDB dagi sessions collectionini beryapmiz.
+    resave: true, // => cookimiz 6 soat mobaynida saqlanar edi agar "resave : true " bolsa shu oraliqda user kirgan bolsa kirgan vaqtidan yana 6 soat hisob lanadi. folse bolsa ozgarmasdan birinchi kirgan vaqtidan hisoblanadi
+    saveUninitialized: true,
+  })
 );
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   const sessionInstance = req.session as T;
   res.locals.member = sessionInstance.member;
   next();
-})
+});
 /** 3- VIEWS  **/
-app.set('views', path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 /** 4- ROUTERS **/
 app.use("/admin", routerAdmin); // BSSR: EJS
-app.use("/", router);          // SPA: REACT
+app.use("/", router); // SPA: REACT
 
-export default app;
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
+
+let summaryClient = 0;
+io.on("connection", (socket) => {
+  summaryClient++;
+  console.log(`Connection & total [${summaryClient}]`);
+
+  socket.on("disconnect", () => {
+    summaryClient--;
+    console.log(`Disconnection & total [${summaryClient}]`);
+  });
+});
+export default server;
