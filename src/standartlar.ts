aa7cb1a82ec0ba937b7c5d81cd967135376d279c
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2025-07-02 14:49:19 +0900

    feat: deploy project via shell & pm2

--- src/standartlar.ts
+++ src/standartlar.ts
@@ -29,3 +29,18 @@ Modern FD => SPA => REACT
   Backend validation
   Database validation
   */
+
+  /*
+  PM2 COMMANDS:
+  
+  pm2 ls
+  pm2 start dist/server.js --name=SUSHI
+  pm2 start "npm run start:prod" --name=SUSHI2
+  pm2 start cmd --name SUSHI-REACT -- /c yarn run start:prod == bu yarn uchun
+  pm2 stop id
+  pm2 delete id
+  pm2 restart id
+  pm2 monit
+  pm2 kill
+
+  */
